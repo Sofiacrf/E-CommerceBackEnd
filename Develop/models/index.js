da81dbfba1908a24bc9d07fc955d7b31c12161ef
Sofia Carolina Rojas Flores <sofia.rojasflo@gmail.com>
2021-06-07 20:40:40 -0500

    updated index file on the models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,8 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    //review if this is right
+    model: Tag
+  },
+  as: 'store_items'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
 
